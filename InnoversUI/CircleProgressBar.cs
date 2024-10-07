0519538ec637fc085a7902471cc28e06c3287c07
Agaffabien <agaffabien@outlook.fr>
2024-10-07 16:05:49 +0000

    Ajoutez des fichiers projet.

---
+++ InnoversUI/CircleProgressBar.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InnoversUI
+{
+    public class CircleProgressBar : Control
+    {
+        #region [PROPRIETIES]
+        
+        public static readonly DependencyProperty SizeProperty =
+          DependencyProperty.Register("Size", typeof(double), typeof(CircleProgressBar), new PropertyMetadata(80.0));
+
+        public double Size
+        {
+            get { return (double)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThicknessProperty =
+         DependencyProperty.Register("Thickness", typeof(double), typeof(CircleProgressBar), new PropertyMetadata(5.0));
+
+        public double Thickness
+        {
+            get { return (double)GetValue(ThicknessProperty); }
+            set { SetValue(ThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsLoadingProperty =
+           DependencyProperty.Register("IsLoading", typeof(bool), typeof(CircleProgressBar), new PropertyMetadata(true));
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+
+
+        public SolidColorBrush ProgressColor
+        {
+            get { return (SolidColorBrush)GetValue(ProgressColorProperty); }
+            set { SetValue(ProgressColorProperty, value); }
+        }
+
+        
+        // Using a DependencyProperty as the backing store for ProgressColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ProgressColorProperty =
+            DependencyProperty.Register("ProgressColor", typeof(SolidColorBrush), typeof(CircleProgressBar), new PropertyMetadata(Brushes.Black));
+
+
+
+
+        public PenLineCap LineCape
+        {
+            get { return (PenLineCap)GetValue(LineCapeProperty); }
+            set { SetValue(LineCapeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineCape.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineCapeProperty =
+            DependencyProperty.Register("LineCape", typeof(PenLineCap), typeof(CircleProgressBar), new PropertyMetadata(PenLineCap.Round));
+
+
+
+        #endregion [PROPRIETIES]
+
+        static CircleProgressBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CircleProgressBar), new FrameworkPropertyMetadata(typeof(CircleProgressBar)));
+        }
+    }
+}
---
+++ InnoversUI/Converters/StrokeDashArrayConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace InnoversUI.Converters
+{
+    internal class StrokeDashArrayConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Length < 2 ||
+                !double.TryParse(values[0].ToString(), out double size) ||
+                !double.TryParse(values[1].ToString(), out double thickness)
+                )
+            {
+                return 0;
+            }
+
+            double Circonference = Math.PI * size;
+            double LineLength = Circonference * 0.75;
+            double GapLength = Circonference - LineLength;
+
+            return new DoubleCollection(new[] {LineLength/thickness, GapLength/thickness});
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

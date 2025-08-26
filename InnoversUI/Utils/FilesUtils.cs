2c3c18ff7f8cee4119639f47a880a89b488aaa65
GEEK <agaffabien@gmail.com>
2025-08-26 08:42:24 +0000

    Ajout FilesUtils

---
+++ InnoversUI/Utils/FilesUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoversUI.Utils
+{
+    public class FilesUtils
+    {
+        public static void MasquerDossierFichier(string Path)
+        {
+            File.SetAttributes(Path, File.GetAttributes(Path) | FileAttributes.System | FileAttributes.Hidden);
+        }
+
+        public static void AfficherDossierFichier(string Path)
+        {
+            //File.SetAttributes(Path, File.GetAttributes(Path) | ~FileAttributes.System | ~FileAttributes.Hidden);
+            FileAttributes attr = File.GetAttributes(Path);
+            attr &= FileAttributes.Hidden;
+            attr &= ~FileAttributes.System;
+            File.SetAttributes(Path, attr);
+        }
+
+        public static void OuvrirDossier(string Path)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", Path);
+        }
+    }
+}

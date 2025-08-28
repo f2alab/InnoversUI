e2688e7c9adfa256d94c7c36b5259daa4073ac55
GEEK <agaffabien@gmail.com>
2025-08-28 17:01:36 +0000

    Ajout de ObservableCollectionExtension et ObservableCollection Utils

---
+++ InnoversUI/Extensions/ObservableCollectionExtension.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace InnoversUI.Extensions
+{
+    public static class ObservableCollectionExtension
+    {
+        public static void Sort<T>(this ObservableCollection<T> collection, Comparison<T> comparison)
+        {
+            var sorted = collection.ToList();
+            sorted.Sort(comparison);
+
+            collection.Clear();
+            foreach (var item in sorted)
+                collection.Add(item);
+        }
+
+        public static void Sort<T, TKey>(this ObservableCollection<T> collection, Func<T, TKey> keySelector)
+        {
+            var sorted = collection.OrderBy(keySelector).ToList();
+
+            collection.Clear();
+            foreach (var item in sorted)
+                collection.Add(item);
+        }
+
+        public static void SortDescending<T, TKey>(this ObservableCollection<T> collection, Func<T, TKey> keySelector)
+        {
+            var sorted = collection.OrderByDescending(keySelector).ToList();
+
+            collection.Clear();
+            foreach (var item in sorted)
+                collection.Add(item);
+        }
+
+        public static void SortInPlace<T>(this ObservableCollection<T> collection, Comparison<T> comparison)
+        {
+            var sorted = collection.OrderBy(x => x, Comparer<T>.Create(comparison)).ToList();
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                var item = sorted[i];
+                int oldIndex = collection.IndexOf(item);
+
+                if (oldIndex != i)
+                    collection.Move(oldIndex, i);
+            }
+        }
+
+        public static void SortInPlace<T, TKey>(this ObservableCollection<T> collection, Func<T, TKey> keySelector)
+        {
+            var sorted = collection.OrderBy(keySelector).ToList();
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                var item = sorted[i];
+                int oldIndex = collection.IndexOf(item);
+
+                if (oldIndex != i)
+                    collection.Move(oldIndex, i);
+            }
+        }
+
+        public static void SortInPlaceOptimized<T>(
+        this ObservableCollection<T> collection,
+        Comparison<T> comparison)
+        {
+            var sorted = collection.OrderBy(x => x, Comparer<T>.Create(comparison)).ToList();
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                var item = sorted[i];
+                int oldIndex = collection.IndexOf(item);
+
+                if (oldIndex != i)
+                {
+                    collection.Move(oldIndex, i);
+                }
+            }
+        }
+
+        public static void SortInPlace<T>(
+        this ObservableCollection<T> collection,
+        params (Func<T, object> KeySelector, bool Descending)[] sortCriteria)
+        {
+            if (sortCriteria == null || sortCriteria.Length == 0)
+                return;
+
+            // Construction de l'expression LINQ dynamique
+            IOrderedEnumerable<T> sorted = sortCriteria[0].Descending
+                ? collection.OrderByDescending(sortCriteria[0].KeySelector)
+                : collection.OrderBy(sortCriteria[0].KeySelector);
+
+            for (int i = 1; i < sortCriteria.Length; i++)
+            {
+                sorted = sortCriteria[i].Descending
+                    ? sorted.ThenByDescending(sortCriteria[i].KeySelector)
+                    : sorted.ThenBy(sortCriteria[i].KeySelector);
+            }
+
+            var sortedList = sorted.ToList();
+
+            // Dictionnaire pour retrouver rapidement les indices
+            var indexLookup = collection
+                .Select((item, idx) => new { item, idx })
+                .ToDictionary(x => x.item, x => x.idx);
+
+            // Déplacement optimisé
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                var item = sortedList[i];
+                int oldIndex = indexLookup[item];
+
+                if (oldIndex != i)
+                {
+                    collection.Move(oldIndex, i);
+
+                    // Mise à jour des indices
+                    for (int j = i; j <= oldIndex; j++)
+                        indexLookup[collection[j]] = j;
+                }
+            }
+        }
+
+        public static async Task SortInPlaceAsync<T>(
+        this ObservableCollection<T> collection,
+        Dispatcher dispatcher,
+        params (Func<T, object> KeySelector, bool Descending)[] sortCriteria)
+        {
+            if (sortCriteria == null || sortCriteria.Length == 0)
+                return;
+
+            // ⚡ Tri en tâche de fond (hors UI thread)
+            var sortedList = await Task.Run(() =>
+            {
+                IOrderedEnumerable<T> sorted = sortCriteria[0].Descending
+                    ? collection.OrderByDescending(sortCriteria[0].KeySelector)
+                    : collection.OrderBy(sortCriteria[0].KeySelector);
+
+                for (int i = 1; i < sortCriteria.Length; i++)
+                {
+                    sorted = sortCriteria[i].Descending
+                        ? sorted.ThenByDescending(sortCriteria[i].KeySelector)
+                        : sorted.ThenBy(sortCriteria[i].KeySelector);
+                }
+
+                return sorted.ToList();
+            });
+
+            // ⚡ Application des Move() sur le thread UI
+            await dispatcher.InvokeAsync(() =>
+            {
+                var indexLookup = collection
+                    .Select((item, idx) => new { item, idx })
+                    .ToDictionary(x => x.item, x => x.idx);
+
+                for (int i = 0; i < sortedList.Count; i++)
+                {
+                    var item = sortedList[i];
+                    int oldIndex = indexLookup[item];
+
+                    if (oldIndex != i)
+                    {
+                        collection.Move(oldIndex, i);
+
+                        for (int j = i; j <= oldIndex; j++)
+                            indexLookup[collection[j]] = j;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ InnoversUI/Utils/CollectionsUtils.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoversUI.Utils
+{
+    public class CollectionsUtils
+    {
+        public static void LoadObservableCollectionWithoutClear<T>(
+           IEnumerable<T> Source,
+           ObservableCollection<T> Cible,
+           Func<T, object> IdSelector,
+           Action<T, T> UpdateItemAction,
+           Func<T, T> CreateItemFunc)
+        {
+            // Supprimer ceux qui n'existent plus
+            var aSupprimer = Cible
+                .Where(c => !Source.Any(s => IdSelector(s).Equals(IdSelector(c))))
+                .ToList();
+
+            foreach (var item in aSupprimer)
+                Cible.Remove(item);
+
+            // Ajouter ou mettre à jour
+            foreach (var itemSource in Source)
+            {
+                var existant = Cible.FirstOrDefault(c => IdSelector(c).Equals(IdSelector(itemSource)));
+
+                if (existant == null)
+                {
+                    // Ajout → nouvelle instance
+                    Cible.Add(CreateItemFunc(itemSource));
+                }
+                else
+                {
+                    // Mise à jour → modifie l’objet existant (pas de perte de sélection)
+                    UpdateItemAction(itemSource, existant);
+                }
+            }
+        }
+
+        public static void LoadObservableCollectionWithoutClear<T, TKey>(
+            IList<T> Source,
+            ObservableCollection<T> Cible,
+            Func<T, TKey> IdSelector,
+            Action<T, T> UpdateItemAction,
+            Func<T, T> CreateItemFunc)
+        {
+            // --- Étape 1 : construire un dictionnaire pour rechercher rapidement ---
+            var dictSource = new Dictionary<TKey, T>();
+            for (int i = 0; i < Source.Count; i++)
+            {
+                var key = IdSelector(Source[i]);
+                if (!dictSource.ContainsKey(key))
+                    dictSource.Add(key, Source[i]);
+            }
+
+            // --- Étape 2 : supprimer les éléments absents ---
+            for (int i = Cible.Count - 1; i >= 0; i--)
+            {
+                var key = IdSelector(Cible[i]);
+                if (!dictSource.ContainsKey(key))
+                {
+                    Cible.RemoveAt(i);
+                }
+            }
+
+            // --- Étape 3 : mise à jour ou ajout ---
+            for (int i = 0; i < Source.Count; i++)
+            {
+                var srcItem = Source[i];
+                var key = IdSelector(srcItem);
+
+                bool found = false;
+                for (int j = 0; j < Cible.Count; j++)
+                {
+                    if (EqualityComparer<TKey>.Default.Equals(IdSelector(Cible[j]), key))
+                    {
+                        // Mise à jour en place => garde la sélection
+                        UpdateItemAction(srcItem, Cible[j]);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    // Ajout => nouvelle instance via factory
+                    Cible.Add(CreateItemFunc(srcItem));
+                }
+            }
+        }
+    }
+}

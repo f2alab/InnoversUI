5d96b4eca3ea3e52040f9b87a6549032882d4ca9
GEEK <agaffabien@gmail.com>
2025-09-02 16:19:31 +0000

    Amelioration ExpanderTile

---
+++ InnoversUI/Converters/TextCapitalize.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InnoversUI.Converters
+{
+    public class TextCapitalize : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(string.IsNullOrEmpty(value.ToString())) return value;
+            return value.ToString().ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
